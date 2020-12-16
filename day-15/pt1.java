a6d96fdf0a15ed5cd5c88421d0961923b077b958
VariableV <student.mhuss@gmail.com>
2020-12-16 16:43:59 -0600

    minor changes

--- day-15/pt1.java
+++ day-15/pt1.java
@@ -5,14 +5,9 @@ class pt1{
     BufferedReader key=new BufferedReader(new FileReader("in.txt"));
     String[] ln = key.readLine().split(",");
     ArrayList<Integer> spoken = new ArrayList<>();
-    HashMap<Integer,Integer> lastspoken = new HashMap<>();
-    HashSet<Integer> spokenalready = new HashSet<>();
-    int $ = 0;
     for(String s : ln){
       spoken.add(Integer.parseInt(s));
-      lastspoken.put(Integer.parseInt(s), $++);
     }
-    int orig = spoken.size();
     for(int i = spoken.size(); i < 20; i++){
       int last = spoken.get(i-1);
       int f0 = -1, f1 = -1;
@@ -33,7 +28,6 @@ class pt1{
         spoken.add(0);
       }
     }
-    System.out.println(spoken);
-    //System.out.println(spoken.get(spoken.size()-1));
+    System.out.println(spoken.get(spoken.size()-1));
   }
 }
